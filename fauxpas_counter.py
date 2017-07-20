1fd4617250337c63cc525207f715ef23d911da6f
andy-grandy <never4ever@mail.ru>
2017-07-20 16:04:56 +0300

    Update fauxpas_counter.py
    Add support of Python 2.x and 3.x

--- fauxpas_counter.py
+++ fauxpas_counter.py
@@ -1,16 +1,17 @@
+from __future__ import print_function
 import json
 
-path = "/Users/admin/jenkins/workspace/iOS-FauxPas/diagnostics_set.json" # paste your path to diagnostics_set.json
+path = "/path/to/diagnostics_set.json" # path to diagnostics_set.json
 
 with open(path) as data_file:
     data = json.load(data_file)
 
-print "Total errors:  %s \n" %len(data["diagnostics"])
+print ("Total errors: {0} \n").format(len(data["diagnostics"]))
 severity = []
 for i in range(0, len(data["diagnostics"])):
     severity.append(data["diagnostics"][i]["severityDescription"])
 
-print "Severity Fatal: %s" %severity.count("Fatal")
-print "Severity Error: %s" %severity.count("Error")
-print "Severity Warning: %s" %severity.count("Warning")
-print "Severity Concern: %s" %severity.count("Concern")
+print ("severity Fatal: {0}").format(severity.count("Fatal"))
+print ("severity Error: {0}").format(severity.count("Error"))
+print ("severity Warning: {0}").format(severity.count("Warning"))
+print ("severity Concern: {0}").format(severity.count("Concern"))
