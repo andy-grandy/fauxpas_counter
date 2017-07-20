635515a50ccda8dc6c1112af43aba848a1024f1c
andy-grandy <never4ever@mail.ru>
2017-07-20 16:07:33 +0300

    Update fauxpas_counter.py
    Delete bad whitespace

--- fauxpas_counter.py
+++ fauxpas_counter.py
@@ -6,12 +6,12 @@ path = "/path/to/diagnostics_set.json" # path to diagnostics_set.json
 with open(path) as data_file:
     data = json.load(data_file)
 
-print ("Total errors: {0} \n").format(len(data["diagnostics"]))
+print("Total errors: {0} \n").format(len(data["diagnostics"]))
 severity = []
 for i in range(0, len(data["diagnostics"])):
     severity.append(data["diagnostics"][i]["severityDescription"])
 
-print ("severity Fatal: {0}").format(severity.count("Fatal"))
-print ("severity Error: {0}").format(severity.count("Error"))
-print ("severity Warning: {0}").format(severity.count("Warning"))
-print ("severity Concern: {0}").format(severity.count("Concern"))
+print("severity Fatal: {0}").format(severity.count("Fatal"))
+print("severity Error: {0}").format(severity.count("Error"))
+print("severity Warning: {0}").format(severity.count("Warning"))
+print("severity Concern: {0}").format(severity.count("Concern"))
